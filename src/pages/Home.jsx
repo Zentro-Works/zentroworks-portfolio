1c5d325f32d665911de5e5cf0f83d1f38634081b
vkvarun <vkvarun4401@gmail.com>
2026-04-21 21:24:44 +0530

    Initial commit - ZentroWorks portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="px-4 md:px-10 py-10">
+      <h1 className="text-4xl font-bold text-center">
+        Welcome to ZentroWorks
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
